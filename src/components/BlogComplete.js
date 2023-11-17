b21277179fd08eac49f3a2bb2c769198b90944c4
NARUTO-MCK <mckcharan9@gmail.com>
2023-11-17 10:34:12 +0530

    Create BlogComplete.js

---
+++ src/components/BlogComplete.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const BlogComplete = () => {
+  return (
+    <div className='BlogCard-whole'>
+        <div className=''>
+        </div>
+    </div>
+  )
+}
+
+export default BlogComplete
