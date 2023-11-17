8e9de3eaffc7ccebc5c962184b7b0cbaad8b46fc
NARUTO-MCK <mckcharan9@gmail.com>
2023-11-17 11:50:48 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import './App.css';
 // import Navbar from './components/Navbar';
 // import Footer from './components/Footer';
 // import UserProfile from './pages/UserProfile';
-import Blogs from './pages/Blogs';
+// import Blogs from './pages/Blogs';
 // import UserProfile from './pages/UserProfile';
+import BlogComplete from './components/BlogComplete';
 import './styles/Style.css';
 
 function App() {
   return (
     <div className='app'>
-      <Blogs />
+      <BlogComplete/>
     </div>
   );
 }
--- src/components/BlogComplete.js
+++ src/components/BlogComplete.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 const BlogComplete = () => {
   return (
-    <div className='BlogCard-whole'>
-        <div className=''>
+    <div className='BlogComplete-whole'>
+        <div className='BlogComplete-matter'>
+          <div className='BlogComplete-head'>
+            <div className='BlogComplete-head-heading'>Lung Cancer Screening Demystified</div>
+            <div className='BlogComplete-head-info'>
+              <div className='BlogComplete-data'><span>Published on &nbsp;</span>17/11/23 <span>by &nbsp;</span>Dr.Ramesh, Psycholigist</div>
+            </div>
+          </div>
+          <div className='BlogComplete-body'></div>
         </div>
     </div>
   )
