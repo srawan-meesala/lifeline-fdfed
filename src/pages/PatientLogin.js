3cf9a9ccbe82eeddad3affdd0ba7766d43d6fead
Srawan Meesala <srawanviz@gmail.com>
2023-11-27 00:48:00 +0530

    commit

--- src/pages/PatientLogin.js
+++ src/pages/PatientLogin.js
@@ -1,11 +1,13 @@
 import React, { useEffect,useState } from "react";
 import axios from "axios";
 import {useNavigate, Link} from 'react-router-dom';
+import { ReactComponent as Logo } from "../images/undraw_remotely_2j6y.svg";
 
 function PatientLogin(){
     const navigate = useNavigate()
     const [username,setusername] = useState('')
     const [password,setpassword] = useState('')
+    const [type,setType] = useState('')
 
     async function submit_login(e){
         e.preventDefault()
@@ -14,13 +16,13 @@ function PatientLogin(){
                 username,password
             })
             .then(res =>{
-                if(res.data == 'exist'){
+                if(res.data === 'exist'){
                     navigate('/userProfile')
                 }
-                else if(res.data == 'doesnot exist'){
+                else if(res.data === 'doesnot exist'){
                     alert('User is not registered')
                 }
-                else if(res.data == 'invalid credentials'){
+                else if(res.data === 'invalid credentials'){
                     alert('Invalid Credentials')
                 }
             })
@@ -32,25 +34,37 @@ function PatientLogin(){
 
 
     return(
-        <div className="patient-login" >
-            <form method="POST" action="/patientLogin" className="patient-login-form">
-                <label>Username</label><b/>
-                <input type="text" placeholder="Username" name="username" onChange={(e) => { setusername(e.target.value) }} required /><br/>
-                <label>Password</label><b/>
-                <input type="password" placeholder="Password" name="password" onChange={(e) => { setpassword(e.target.value) }} required/><br/>
-                <input type="submit" onClick={submit_login} />
-            </form>
-
-            <br />
-            <p>OR</p>
-            <br />
-
-            <Link to="/PatientRegister">Log In</Link>
+        <div className="PatientLogin-whole" >
+            <div className="PatientLogin-left">
+                <div className="PatientLogin-logo ">Lifeline<span className="PatientLogin-logo-span">.</span>&nbsp;<span className="PatientLogin-logo-side">Login</span></div>
+                <Logo className="PatientLogin-image"/>
+            </div>
+            <div className="PatientLogin-right">
+                <form method="POST" action="/patientLogin" className="PatientLogin-form">
+                    <div className="PatientLogin-username PatientLogin-input">
+                        <label htmlFor="username">Username</label><b/>
+                        <input type="text" placeholder="Username" name="username" onChange={(e) => { setusername(e.target.value) }} required /><br/>
+                    </div>
+                    <div className="PatientLogin-password PatientLogin-input">
+                        <label htmlFor="password">Password</label><b/>
+                        <input type="password" placeholder="Password" name="password" onChange={(e) => { setpassword(e.target.value) }} required/><br/>
+                    </div>
+                    <div className="PatientLogin-type PatientLogin-input">
+                        <label htmlFor="password">Type of User</label><b/>
+                        <select name="type" onChange={(e) => { setType(e.target.value) }} required>
+                            <option value="patient" selected>Patient</option>
+                            <option value="doctor">Doctor</option>
+                            <option value="hospital">Hospital</option>
+                            <option value="admin">Admin</option>
+                        </select><br/>
+                    </div>
+                    <button type="submit" onClick={submit_login} >Log In</button>
+                </form>
+                <Link className="PatientLogin-right-register" to="/PatientRegister">Haven't Registered yet?</Link>
+            </div>
+            
         </div>
     )
 }
 
 export default PatientLogin
-
-
-
