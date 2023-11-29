257cf3a6165090011f27396981b0d541c96ebfa9
NARUTO-MCK <mckcharan9@gmail.com>
2023-11-29 22:37:59 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -7,13 +7,14 @@ import Blogs from './pages/Blogs';
 import PatientRegister from './pages/PatientRegister';
 import Login from './pages/Login';
 import DocRegister from './pages/DocRegister';
+import BlogForm from './pages/BlogForm';
 
 function App() {
   return (
     <div className='app'>
        <Router>
         <Routes>
-          <Route path="/" element={<Blogs/>}/>
+          <Route path="/" element={<BlogForm/>}/>
           <Route path="/login" element={<Login/>}/>
           <Route path="/patientRegister" element={<PatientRegister/>}/>
           <Route path="/docRegister" element={<DocRegister/>}/>
