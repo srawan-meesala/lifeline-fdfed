28236e7a2cc79d1960c527db4a6b81281a800d95
Srawan Meesala <srawanviz@gmail.com>
2023-11-29 22:44:01 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from 'react'
-import UserProfile from './pages/UserProfile';
 import './styles/Style.css';
-import Blogs from './pages/Blogs';
-import PatientRegister from './pages/PatientRegister';
-import Login from './pages/Login';
-import DocRegister from './pages/DocRegister';
-import BlogForm from './pages/BlogForm';
+import LandingRoutes from './Routes/LandingRoutes';
+import ProfileRoutes from './Routes/ProfileRoutes';
 
 function App() {
   return (
     <div className='app'>
-       <Router>
-        <Routes>
-          <Route path="/" element={<BlogForm/>}/>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/patientRegister" element={<PatientRegister/>}/>
-          <Route path="/docRegister" element={<DocRegister/>}/>
-          <Route path="/userProfile/:username" element={<UserProfile/>}/>
-        </Routes>
-      </Router>
+      <LandingRoutes />
+      <ProfileRoutes />
     </div>
   );
 }
--- src/Routes/LandingRoutes.js
+++ src/Routes/LandingRoutes.js
@@ -6,17 +6,19 @@ import Landing from '../pages/Landing';
 import AboutUs from '../pages/AboutUs';
 import PatientRegister from '../pages/PatientRegister';
 import Blogs from '../pages/Blogs';
+import BlogForm from '../pages/BlogForm';
 
 const LandingRoutes = () => {
   return (
     <Router>
         <Routes>
-        <Route path="/" element={<Landing/>}/>
-        <Route path="/home" element={<Home/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/about" element={<AboutUs/>}/>
-        <Route path="/patientRegister" element={<PatientRegister/>}/>
-        <Route path="/blogs" element={<Blogs/>}/>
+          <Route path="/" element={<Landing/>}/>
+          <Route path="/home" element={<Home/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/about" element={<AboutUs/>}/>
+          <Route path="/patientRegister" element={<PatientRegister/>}/>
+          <Route path="/blogs" element={<Blogs/>}/>
+          <Route path="/blogform" element={<BlogForm/>}/>
         </Routes>
     </Router>
   )
