9ebbb06a2936b1ab4d6328df2a169f5029fdb21d
Srawan Meesala <srawanviz@gmail.com>
2023-11-29 22:27:09 +0530

    commit

--- src/pages/PatientRegister.js
+++ src/pages/PatientRegister.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react"
 import axios from "axios"
 import { useNavigate, Link } from "react-router-dom"
+import { ReactComponent as Logo } from "../images/undraw_remotely_2j6y.svg";
 
 
 function PatientRegister() {
@@ -49,35 +50,41 @@ function PatientRegister() {
 
 
     return (
-        <div className="patient-register">
+        <div className="PatientLogin-whole">
 
-            <h1>Register</h1>
+            <div className="PatientLogin-left">
+                <div className="PatientLogin-logo ">Lifeline<span className="PatientLogin-logo-span">.</span>&nbsp;<span className="PatientLogin-logo-side">Registration</span></div>
+                <Logo className="PatientLogin-image"/>
+            </div>
+            <div className="PatientLogin-right">
+                <form method="POST" action="/patientRegister" className="patient-register-form">
+                    <label >First Name</label><b/>
+                    <input type="text" onChange={(e) => { setfirstName(e.target.value) }} name="firstName" placeholder="First Name" required  />
+                    <label htmlFor="lastname">Last Name</label><b/>
+                    <input type="text" onChange={(e) => { setlastName(e.target.value) }} name="lastName" placeholder="Last Name" required  />
+                    <label >Mobile Number</label><b/>
+                    <input type="number" onChange={(e) => { setmobileNumber(e.target.value) }} name="mobileNumber" placeholder="mobileNumber" required  />
+                    <label >Email ID</label><b/>
+                    <input type="email" onChange={(e) => { setmailID(e.target.value) }} name="mailID" placeholder="mailID" required  />
+                    <label >Date of Birth</label><b/>
+                    <input type="date" onChange={(e) => { setdob(e.target.value) }} name="dob" required  />
+                    <label >Occupation</label><b/>
+                    <input type="text" onChange={(e) => { setoccupation(e.target.value) }} name="occupation" placeholder="occupation" required  />
+                    <label >Blood Group</label><b/>
+                    <input type="text" onChange={(e) => { setbloodGroup(e.target.value) }} name="bloodGroup" placeholder="bloodGroup" required  />
+                    <label >Marital Status</label><b/>
+                    <input type="text" onChange={(e) => { setmaritalStatus(e.target.value) }} name="maritalStatus" placeholder="maritalStatus" required  />
+                    <label >Gender</label><b/>
+                    <input type="text" onChange={(e) => { setgender(e.target.value) }} name="gender" placeholder="gender" required  />
+                    <label >Username</label><b/>
+                    <input type="text" onChange={(e) => { setUsername(e.target.value) }} name="Username" placeholder="Username" required  />
+                    <label >Password</label><b/>
+                    <input type="password" onChange={(e) => { setPassword(e.target.value) }} name="Password" placeholder="Password" required  />
+                    <input type="submit" onClick={submit_register} />
 
-            <form method="POST" action="/patientRegister" className="patient-register-form">
-                <label >FirstName</label><b/>
-                <input type="text" onChange={(e) => { setfirstName(e.target.value) }} name="firstName" placeholder="firstName" required  /><br/>
-                <label >lastName</label><b/>
-                <input type="text" onChange={(e) => { setlastName(e.target.value) }} name="lastName" placeholder="lastName" required  /><br/>
-                <label >Mobile Number</label><b/>
-                <input type="number" onChange={(e) => { setmobileNumber(e.target.value) }} name="mobileNumber" placeholder="mobileNumber" required  /><br/>
-                <label >Email ID</label><b/>
-                <input type="email" onChange={(e) => { setmailID(e.target.value) }} name="mailID" placeholder="mailID" required  /><br/>
-                <label >Date of Birth</label><b/>
-                <input type="date" onChange={(e) => { setdob(e.target.value) }} name="dob" required  /><br/>
-                <label >Occupation</label><b/>
-                <input type="text" onChange={(e) => { setoccupation(e.target.value) }} name="occupation" placeholder="occupation" required  /><br/>
-                <label >Blood Group</label><b/>
-                <input type="text" onChange={(e) => { setbloodGroup(e.target.value) }} name="bloodGroup" placeholder="bloodGroup" required  /><br/>
-                <label >Marital Status</label><b/>
-                <input type="text" onChange={(e) => { setmaritalStatus(e.target.value) }} name="maritalStatus" placeholder="maritalStatus" required  /><br/>
-                <label >Gender</label><b/>
-                <input type="text" onChange={(e) => { setgender(e.target.value) }} name="gender" placeholder="gender" required  /><br/>
-                <label >Username</label><b/>
-                <input type="text" onChange={(e) => { setUsername(e.target.value) }} name="Username" placeholder="Username" required  /><br/>
-                <label >Password</label><b/>
-                <input type="password" onChange={(e) => { setPassword(e.target.value) }} name="Password" placeholder="Password" required  /><br/>
-                <input type="submit" onClick={submit_register} />
+                </form>
 
+<<<<<<< Updated upstream
             </form>
 
             <br />
@@ -85,7 +92,14 @@ function PatientRegister() {
             <br />
 
             <Link to="/login">login </Link>
+=======
+                <br />
+                <p>OR</p>
+                <br />
+>>>>>>> Stashed changes
 
+                <Link to="/PatientLogin">login </Link>
+            </div>
         </div>
     )
 }
