9b7a50c61e773a6a9459c3dc8e5670011b02888f
NARUTO-MCK <mckcharan9@gmail.com>
2023-11-29 22:29:45 +0530

    commit

--- src/pages/BlogForm.js
+++ src/pages/BlogForm.js
@@ -1,14 +1,29 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 function BlogForm() {
   return (
-    <div>
-        <form>
-            <label>Title</label>
-            <input type='text' />
-            <label>Blog</label>
-            <input type='text' />
-        </form>
+    <div className='Blogform-whole'>
+        <Navbar/>
+            <div className='Blogform-upper'>
+                <div className='Blogform-headline'>Write your Blogs</div>
+            </div>
+            <div className='Blogform-mid'>
+                <div className='Blogform-mid-int'>
+                    <div className='Blogform-form-div'>
+                        <form>
+                            <label>Title</label>
+                            <input type='text' placeholder='Your Blog Title'/>
+                            <label>Blog</label>
+                            <textarea type='text' placeholder='Your Blog'>
+                            </textarea>
+                        </form>
+                    </div>
+                    <div className='Blogform-remaining'>
+                    <button className='Blogform-remaining-button'>Upload</button>
+                    </div>
+                </div>
+            </div>
     </div>
   )
 }
