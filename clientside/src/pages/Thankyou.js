a4a7e6d998f1562f462eeef088fd58e2d33cc2d1
NARUTO-MCK <mckcharan9@gmail.com>
2023-12-06 14:18:45 +0530

    commit

--- clientside/src/pages/Thankyou.js
+++ clientside/src/pages/Thankyou.js
@@ -2,19 +2,19 @@ import React from 'react'
 
 function Thankyou() {
   return (
-    <div><div class="containerr">
-    <div class="logo">Lifeline.</div>
-    <div class="content">
-        <div class="l1">Your Payment has been approved and the appointment is confirmed.</div>
-        <div class="l2">Thank you for choosing Lifeline as your medical partner.</div>
-        <div class="l3"> We will assure you the best service possible.</div>
+    <div><div class="Thankyou-containerr">
+    <div class="Thankyou-logo">Lifeline.</div>
+    <div class="Thankyou-content">
+        <div class="Thankyou-l1">Your Payment has been approved and the appointment is confirmed.</div>
+        <div class="Thankyou-l2">Thank you for choosing Lifeline as your medical partner.</div>
+        <div class="Thankyou-l3"> We will assure you the best service possible.</div>
     </div>
-    <div class="btn-home">
-        <button><a href="home-page">Back to Home</a></button>
+    <div class="Thankyou-btn-home">
+        <button><a href="Thankyou-home-page">Back to Home</a></button>
     </div>
 </div>
-<div class="background">
-    <svg class="border shadow-md" viewBox="0 0 1081.52 608.355"  xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none"><rect class="bg" id="bg" x="0" y="0" width="1081.52" height="608.355" fill="#ffffff"></rect><g transform="rotate(0 540.76 304.1775)"><path d="M -540.76 498.36 S -280.38 446.36
+<div class="Thankyou-background">
+    <svg class="Thankyou-border shadow-md" viewBox="0 0 1081.52 608.355"  xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none"><rect class="bg" id="bg" x="0" y="0" width="1081.52" height="608.355" fill="#ffffff"></rect><g transform="rotate(0 540.76 304.1775)"><path d="M -540.76 498.36 S -280.38 446.36
         0.00 498.36 83.76 438.36
         540.76 498.36 801.14 497.36
         1081.52 498.36 1178.28 446.36
