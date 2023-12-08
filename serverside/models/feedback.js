44b77b82aa6835cf075f4b1264c26dff0635dc0a
Srawan Meesala <srawanviz@gmail.com>
2023-12-08 10:00:20 +0530

    Update feedback.js

--- serverside/models/feedback.js
+++ serverside/models/feedback.js
@@ -3,10 +3,8 @@ const mongoose = require('mongoose');
 const feedbackSchema = new mongoose.Schema({
     name: String,
     email: String,
-    phone: String,
     message: String,
   });
   
-  const Feedback = mongoose.model('Feedback', feedbackSchema);
-
+const Feedback = mongoose.model('Feedback', feedbackSchema);
 module.exports =  Feedback
