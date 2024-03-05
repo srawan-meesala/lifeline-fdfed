aa3f6977774137b0aa178ed690e802e1c4008ac4
Srawan Meesala <srawanviz@gmail.com>
2024-03-05 16:15:14 +0530

    commit

--- clientside/src/components/DoctorProfile/DoctorAppointments.js
+++ clientside/src/components/DoctorProfile/DoctorAppointments.js
@@ -1,42 +1,98 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-const DoctorAppointments = ({userDetails,appointments}) => {
+const DoctorAppointments = ({ userDetails, appointments }) => {
+    const [appointmentsPresent, setAppointmentsPresent] = useState(appointments);
+    const [filter, setFilter] = useState('All');
+    const [pastFilter, setPastFilter] = useState('All');
+
+    useEffect(() => {
+        filterAppointments();
+    }, [filter, pastFilter, appointments]);
+
+    const filterAppointments = () => {
+        const today = new Date();
+        switch (filter) {
+            case 'All':
+                setAppointmentsPresent(appointments);
+                break;
+            case 'Past':
+                const pastAppointments = appointments.filter(appointment => {
+                    const appointmentDate = new Date(appointment.Date);
+                    const oneMonthAgo = new Date(today.getFullYear(), today.getMonth() - 1, today.getDate());
+                    return (pastFilter === 'All' && appointmentDate < today) || (pastFilter === 'PastMonth' && appointmentDate >= oneMonthAgo && appointmentDate < today) || (pastFilter === 'PastWeek' && appointmentDate >= new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7) && appointmentDate < today);
+                });
+                setAppointmentsPresent(pastAppointments);
+                break;
+            case 'Future':
+                const futureAppointments = appointments.filter(appointment => {
+                    const appointmentDate = new Date(appointment.Date);
+                    return appointmentDate >= today;
+                });
+                setAppointmentsPresent(futureAppointments);
+                break;
+            default:
+                setAppointmentsPresent(appointments);
+                break;
+        }
+    };
+
+    const handleFilterChange = e => {
+        setFilter(e.target.value);
+    };
+
+    const handlePastFilterChange = e => {
+        setPastFilter(e.target.value);
+    };
 
     return (
-        <div className='UserProfile-right'>
-            <div className='UserProfile-top'>
-            <div className='HospitalProfile-dashboard'>
-                <div>Welcome, <span>{userDetails.name}</span></div>
+        <div className="UserProfile-right">
+            <div className="UserProfile-top">
+                <div className="HospitalProfile-dashboard">
+                    <div>
+                        Welcome, <span>{userDetails.name}</span>
+                    </div>
+                </div>
             </div>
-            </div>
-            <div className='UserProfile-appointments'>
+            <div className="UserProfile-appointments">
                 <div className="UserProfile-appointments-title">Your Appointments</div>
+                <div className="UserProfile-appointments-title">
+                    <select name="filter" onChange={handleFilterChange}>
+                        <option value="All">All</option>
+                        <option value="Past">Past</option>
+                        <option value="Future">Future</option>
+                    </select>
+                    {filter === 'Past' && (
+                        <select name="pastFilter" onChange={handlePastFilterChange}>
+                            <option value="All">All</option>
+                            <option value="PastMonth">Past Month</option>
+                            <option value="PastWeek">Past Week</option>
+                        </select>
+                    )}
+                </div>
                 <div className="UserProfile-appointments-cards">
-                    
-                    {appointments.length === 0 ? (
+                    {appointmentsPresent.length === 0 ? (
                         <p>No Appointments Found.</p>
                     ) : (
-                        appointments.map((appointment) => (
+                        appointmentsPresent.map(appointment => (
                             <div key={appointment.appointmentID} className="UserProfile-appointments-card">
-                                <div className='UserProfile-appointments-card'>
-                                    <div className='UserProfile-appointments-card-name'>
+                                <div className="UserProfile-appointments-card">
+                                    <div className="UserProfile-appointments-card-name">
                                         Patient Name: <span>{appointment.PatientName}</span>
                                     </div>
-                                    <div className='UserProfile-appointments-card-name'>
-                                        Date: <span>{(appointment.Date)}</span>
+                                    <div className="UserProfile-appointments-card-name">
+                                        Date: <span>{appointment.Date}</span>
                                     </div>
-                                    <div className='UserProfile-appointments-card-name'>
-                                        Timeslot: <span>{(appointment.Timeslot)}</span>
+                                    <div className="UserProfile-appointments-card-name">
+                                        Timeslot: <span>{appointment.Timeslot}</span>
                                     </div>
                                 </div>
                             </div>
                         ))
                     )}
-                    
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default DoctorAppointments
+export default DoctorAppointments;
--- clientside/src/components/UserProfile/UserAppointments.js
+++ clientside/src/components/UserProfile/UserAppointments.js
@@ -1,36 +1,34 @@
-import React, { useEffect,useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
 const UserAppointments = ({ userDetails, appointments }) => {
-
-    const [appointmentsPresent, setAppointmentsPresent] = useState(appointments)
-    const [filter, setFilter] = useState('All')
+    const [appointmentsPresent, setAppointmentsPresent] = useState(appointments);
+    const [filter, setFilter] = useState('All');
+    const [pastFilter, setPastFilter] = useState('All');
 
     useEffect(() => {
         filterAppointments();
-    }, [filter]);
+    }, [filter, pastFilter, appointments]);
 
     const filterAppointments = () => {
+        const today = new Date();
         switch (filter) {
             case 'All':
                 setAppointmentsPresent(appointments);
                 break;
-            case 'lastWeek':
-            const lastWeekAppointments = appointments.filter(appointment => {
-                const appointmentDate = new Date(appointment.Date);
-                const today = new Date();
-                const lastWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate()-7);
-                return appointmentDate >= lastWeek;
-            });
-            setAppointmentsPresent(lastWeekAppointments);
-            break;
-            case 'lastMonth':
-                const lastMonthAppointments = appointments.filter(appointment => {
+            case 'Past':
+                const pastAppointments = appointments.filter(appointment => {
                     const appointmentDate = new Date(appointment.Date);
-                    const today = new Date();
-                    const lastMonth = new Date(today.getFullYear(), today.getMonth() - 1, today.getDate());
-                    return appointmentDate >= lastMonth;
+                    const oneMonthAgo = new Date(today.getFullYear(), today.getMonth() - 1, today.getDate());
+                    return (pastFilter === 'All' && appointmentDate < today) || (pastFilter === 'PastMonth' && appointmentDate >= oneMonthAgo && appointmentDate < today) || (pastFilter === 'PastWeek' && appointmentDate >= new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7) && appointmentDate < today);
                 });
-                setAppointmentsPresent(lastMonthAppointments);
+                setAppointmentsPresent(pastAppointments);
+                break;
+            case 'Future':
+                const futureAppointments = appointments.filter(appointment => {
+                    const appointmentDate = new Date(appointment.Date);
+                    return appointmentDate >= today;
+                });
+                setAppointmentsPresent(futureAppointments);
                 break;
             default:
                 setAppointmentsPresent(appointments);
@@ -38,58 +36,69 @@ const UserAppointments = ({ userDetails, appointments }) => {
         }
     };
 
-    const handleFilterChange = (e) => {
+    const handleFilterChange = e => {
         setFilter(e.target.value);
     };
 
+    const handlePastFilterChange = e => {
+        setPastFilter(e.target.value);
+    };
+
     return (
-        <div className='UserProfile-right'>
-            <div className='UserProfile-top'>
-                <div className='HospitalProfile-dashboard'>
-                    <div>Welcome, <span>{userDetails.name}</span></div>
+        <div className="UserProfile-right">
+            <div className="UserProfile-top">
+                <div className="HospitalProfile-dashboard">
+                    <div>
+                        Welcome, <span>{userDetails.name}</span>
+                    </div>
                 </div>
             </div>
-            <div className='UserProfile-appointments'>
+            <div className="UserProfile-appointments">
                 <div className="UserProfile-appointments-title">Your Appointments</div>
                 <div className="UserProfile-appointments-title">
                     <select name="filter" onChange={handleFilterChange}>
                         <option value="All">All</option>
-                        <option value="lastWeek">Last Week</option>
-                        <option value="lastMonth">Last Month</option>
+                        <option value="Past">Past</option>
+                        <option value="Future">Upcoming</option>
                     </select>
+                    {filter === 'Past' && (
+                        <select name="pastFilter" onChange={handlePastFilterChange}>
+                            <option value="All">All</option>
+                            <option value="PastMonth">Past Month</option>
+                            <option value="PastWeek">Past Week</option>
+                        </select>
+                    )}
                 </div>
                 <div className="UserProfile-appointments-cards">
-
                     {appointmentsPresent.length === 0 ? (
                         <p>No Appointments Found.</p>
                     ) : (
-                        appointmentsPresent.map((appointment) => (
+                        appointmentsPresent.map(appointment => (
                             <div key={appointment.appointmentID} className="UserProfile-appointments-card">
-                                <div className='UserProfile-appointments-card'>
-                                    <div className='UserProfile-appointments-card-name'>
+                                <div className="UserProfile-appointments-card">
+                                    <div className="UserProfile-appointments-card-name">
                                         Doctor Name: <span>{appointment.docName}</span>
                                     </div>
-                                    <div className='UserProfile-appointments-card-name'>
-                                        Date: <span>{(appointment.Date)}</span>
+                                    <div className="UserProfile-appointments-card-name">
+                                        Date: <span>{appointment.Date}</span>
                                     </div>
-                                    <div className='UserProfile-appointments-card-name'>
-                                        Timeslot: <span>{(appointment.Timeslot)}</span>
+                                    <div className="UserProfile-appointments-card-name">
+                                        Timeslot: <span>{appointment.Timeslot}</span>
                                     </div>
-                                    <div className='UserProfile-appointments-card-name'>
-                                        Appointment Fees: <span>{(appointment.Fee)}</span>
+                                    <div className="UserProfile-appointments-card-name">
+                                        Appointment Fees: <span>{appointment.Fee}</span>
                                     </div>
-                                    <div className='UserProfile-appointments-card-name'>
-                                        Hospital Name: <span>{(appointment.hospName)}</span>
+                                    <div className="UserProfile-appointments-card-name">
+                                        Hospital Name: <span>{appointment.hospName}</span>
                                     </div>
                                 </div>
                             </div>
                         ))
                     )}
-
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default UserAppointments
+export default UserAppointments;
