0721db7c21d9b20e9ad29e463ac29880236460ed
Srawan Meesala <srawanviz@gmail.com>
2024-04-30 02:28:05 +0530

    Update Searchbar.js

--- clientside/src/components/Searchbar.js
+++ clientside/src/components/Searchbar.js
@@ -1,28 +1,25 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { FaSearch } from 'react-icons/fa';
-import '../styles/Searchbar.css'
+import '../styles/Searchbar.css';
 
 const Searchbar = ({ onSearch }) => {
   const [searchQuery, setSearchQuery] = useState('');
 
   const handleSearch = async (e) => {
     e.preventDefault();
+    
     if (searchQuery.trim() === '') {
-      try {
-        const response = await axios.post(`http://localhost:8000/searchDoctors/${searchQuery}`);
-        onSearch(response.data);
-      } catch (error) {
-        console.error('Error searching doctors:', error);
-      }
-    }else{
+      console.error('Search query cannot be empty');
+      return; // Don't proceed with empty search query
+    }
 
-      try {
-        const response = await axios.post(`http://localhost:8000/searchDoctors/${searchQuery}`);
-        onSearch(response.data);
-      } catch (error) {
-        console.error('Error searching doctors:', error);
-      }
+    try {
+      const response = await axios.post(`http://localhost:8000/searchDoctors/${searchQuery}`);
+      onSearch(response.data);
+    } catch (error) {
+      console.error('Error searching doctors:', error);
+      // Display user-friendly error message in the UI if needed
     }
   };
 
